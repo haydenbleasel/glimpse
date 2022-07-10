495d052c87959c76e265feac2ff25579b483b36e
Hayden Bleasel <hello@haydenbleasel.com>
2022-07-10 20:20:47 +1000

    Remove redundant next config

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 const { createSecureHeaders } = require('next-secure-headers');
-const withPlugins = require('next-compose-plugins');
-const withPWA = require('next-pwa');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+module.exports = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
@@ -23,54 +21,4 @@ const nextConfig = {
       },
     ];
   },
-  async redirects() {
-    return [
-      {
-        source: '/work/presumi',
-        destination: '/blog/presumi',
-        permanent: true,
-      },
-      {
-        source: '/thoughts/presumi',
-        destination: '/blog/presumi',
-        permanent: true,
-      },
-      {
-        source: '/presumi',
-        destination: '/blog/presumi',
-        permanent: true,
-      },
-      {
-        source: '/awards',
-        destination: '/',
-        permanent: true,
-      },
-      {
-        source: '/contact',
-        destination: 'https://twitter.com/haydenbleasel',
-        permanent: true,
-      },
-      {
-        source: '/blog/jellypepper',
-        destination: '/work/jellypepper',
-        permanent: true,
-      },
-      {
-        source: '/journal/how-to-growth-hack-your-resume',
-        destination: '/',
-        permanent: true,
-      },
-    ];
-  },
 };
-
-const pwaConfig = {
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    dynamicStartUrl: false,
-    mode: process.env.NODE_ENV,
-  },
-};
-
-module.exports = withPlugins([[withPWA, pwaConfig]], nextConfig);
