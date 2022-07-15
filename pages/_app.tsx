ae595ce49fccbad09e8a738485c6ab5f3ca2af19
Hayden Bleasel <hello@haydenbleasel.com>
2022-07-15 20:35:18 +1000

    Workaround TS issue

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,10 @@ import type { AppProps } from 'next/app';
 import type { FC } from 'react';
 import '../styles/globals.css';
 
-const MyApp: FC<AppProps> = ({ Component, pageProps }) => (
-  <Component {...pageProps} />
-);
+const MyApp: FC<AppProps> = ({ Component, pageProps }) => {
+  const ComponentX = Component as unknown as FC<AppProps>;
+
+  return <ComponentX {...pageProps} />;
+};
 
 export default MyApp;
