79cd3f3c3de0970f6e72d7f49e3d68d344308dd8
Hayden Bleasel <hello@haydenbleasel.com>
2022-07-15 20:11:47 +1000

    Migrate old files

---
+++ pages/api/screenshot.ts
@@ -0,0 +1,63 @@
+import type { NextApiHandler } from 'next';
+import type { PuppeteerLifeCycleEvent } from 'puppeteer-core';
+import puppeteer from 'puppeteer-core';
+import chrome from 'chrome-aws-lambda';
+
+export type ScreenshotResponse = {
+  error?: string;
+  image?: string;
+};
+
+type RequestData = {
+  url?: string;
+  height?: number;
+  width?: number;
+  waitUntil?: PuppeteerLifeCycleEvent;
+};
+
+const handler: NextApiHandler<ScreenshotResponse> = async (req, res) => {
+  const { url, width = 1920, height = 1080 } = req.body as RequestData;
+
+  if (!url) {
+    res.status(400).json({ error: 'No URL specified' });
+    return;
+  }
+
+  try {
+    const browser = await puppeteer.launch(
+      process.env.AWS_EXECUTION_ENV
+        ? {
+            args: chrome.args,
+            executablePath: await chrome.executablePath,
+            headless: chrome.headless,
+          }
+        : {
+            args: [],
+            executablePath:
+              '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+          }
+    );
+
+    const page = await browser.newPage();
+    await page.setViewport({ width, height });
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    const image = (await page.screenshot({
+      type: 'png',
+      encoding: 'base64',
+    })) as string;
+
+    await browser.close();
+
+    if (!image) {
+      res.status(400).json({ error: 'No image found' });
+      return;
+    }
+
+    res.status(200).json({ image });
+  } catch (error) {
+    const message = error instanceof Error ? error.message : (error as string);
+    res.status(500).json({ error: message });
+  }
+};
+
+export default handler;
--- server/index.ts
+++ server/index.ts
@@ -1,22 +1,32 @@
-import { createServer } from 'http'
-import { parse } from 'url'
-import next from 'next'
+import { createServer } from 'http';
+import { parse } from 'url';
+import next from 'next';
 
-const port = parseInt(process.env.PORT || '3000', 10)
-const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
-const handle = app.getRequestHandler()
+const port = parseInt(process.env.PORT ?? '3000', 10);
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
 
-app.prepare().then(() => {
-  createServer((req, res) => {
-    const parsedUrl = parse(req.url!, true)
-    handle(req, res, parsedUrl)
-  }).listen(port)
+app
+  .prepare()
+  .then(() => {
+    createServer((req, res) => {
+      if (!req.url) {
+        return;
+      }
+      const parsedUrl = parse(req.url, true);
+      handle(req, res, parsedUrl).catch((error) => {
+        const message =
+          error instanceof Error ? error.message : (error as string);
 
-  // tslint:disable-next-line:no-console
-  console.log(
-    `> Server listening at http://localhost:${port} as ${
-      dev ? 'development' : process.env.NODE_ENV
-    }`
-  )
-})
+        throw new Error(message);
+      });
+    }).listen(port);
+
+    console.log(
+      `> Server listening at http://localhost:${port} as ${
+        dev ? 'development' : process.env.NODE_ENV
+      }`
+    );
+  })
+  .catch(console.error);
